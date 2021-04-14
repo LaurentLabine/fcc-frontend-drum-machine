e47661dbbd01350675c8e1d3c532a6982366db2a
Laurent Labine <laurentlabine@gmail.com>
2021-04-14 15:39:54 +0200

    Initialized project using create-react-app
    Also added gh-pages and associated scripts

---
+++ src/drum/component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Drum = () => {
+    return ( 
+        <React.Fragment>
+            <h1>this is my drum</h1>
+        </React.Fragment>
+    );
+}
+
+export default Drum; 
